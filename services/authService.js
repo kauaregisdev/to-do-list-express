10c2a7b8602a1ecde9fe199312feacd092f0cd93
Kauã Régis <kauarlc@hotmail.com>
2025-06-19 13:46:15 -0300

    bcrypt instalado e arquivos renomeados

--- services/authService.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET_KEY;
-
-function generateToken(payload) {
-    return jwt.sign(payload, SECRET, {expiresIn: '2h'});
-}
-
-module.exports = generateToken;
