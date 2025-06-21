b5e30b712eb696c1253a4d03d743d896926e9cdc
Kauã Régis <kauarlc@hotmail.com>
2025-06-21 20:11:31 -0300

    CSS na página 404

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,7 @@
 export default function NotFoundPage() {
-    return <h1>404 - Page Not Found</h1>;
+    return (
+        <div className="max-w-3xl mx-auto bg-white p-8 rounded-2xl shadow-lg mt-10">
+            <h1 className="text-3xl font-bold mb-6 text-gray-800">404 - Page Not Found</h1>
+        </div>
+    );
 }
